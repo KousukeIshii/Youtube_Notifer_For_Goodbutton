ee08f2ef8695023de28c104cfdb069833e68a8a9
石井康介 <faitayahhuu@yahoo.co.jp>
2019-01-28 05:33:20 +0900

    remove unnecessary files

--- Youtube_Notifier.py
+++ Youtube_Notifier.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup as bs4
 
 last_good = "0"
 cur_good = "0"
-
+test="https://www.youtube.com/channel/UCyMW-_pfjsTG76c9tP5SErg"
 
 app = Flask(__name__)
 
@@ -15,18 +15,21 @@ def hello_world():
 	global last_good
 	global cur_good
 	last_good = cur_good
-	url = "https://www.youtube.com/channel/UCyMW-_pfjsTG76c9tP5SErg"
+	url = test
 	html = requests.get(url)
 	soup = bs4(html.text, "lxml")
 	a = soup.select_one(".expanded-shelf > .branded-page-module-title > a > span > span")
-	if format(a.text == u"ライブ配信中") :
+	if a is not None :
 		live_element = soup.select_one("div.expanded-shelf.shelf-item > ul > li > div > div.yt-lockup-dismissable > div.yt-lockup-content > h3 > a")
 		stream_url = "https://www.youtube.com" + live_element.get("href")
 		html = requests.get(stream_url)
 		soup = bs4(html.text, "lxml")
 		b = soup.select_one("#watch8-sentiment-actions > span > span:nth-of-type(2) > button > span")
 		cur_good = b.text
-		return render_template('kameme.html', last_good = last_good, cur_good = cur_good)
+		return render_template('Notifier.html', last_good = last_good, cur_good = cur_good)
+	last_good = "0"
+	cur_good = "0"
+	return render_template('Notifier.html',last_good = '配信中',cur_good = 'じゃないよ')
 
 
 if __name__ == '__main__':
